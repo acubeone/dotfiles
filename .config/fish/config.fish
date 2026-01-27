d24f16764270a1cb2c010684d3c8916ea022a7dd
aCube <player559001@gmail.com>
2026-01-26 22:22:39 -0300

    Update Fish and Kitty config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -x SDL_GAMECONTROLLER_IGNORE_DEVICES 0079/0006
+
 set -g theme_color_scheme nord
 set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
@@ -19,6 +21,3 @@ function starship_transient_rprompt_func
 end
 starship init fish | source
 enable_transience
-
-set -gx CC clang
-set -gx CXX clang++
